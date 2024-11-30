f12cd90210caefc30312d827fbaa0ca84e29a984
M-Abdeltawab <s-mahmoud.twaab@zewailcity.edu.eg>
2024-11-30 14:26:23 +0200

    Made some edits on pages

--- apizza/urls.py
+++ apizza/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin  
 from django.urls import path, include  
-from resadmin.views import home  # Import the new home view  
-
 urlpatterns = [  
-    path('', home, name='home'),  # Add the home view for the root URL  
     path('admin/', admin.site.urls),  
-    path('api/', include('resadmin.urls')),  # Include the resadmin API URLs  
     path('api/', include('users.urls')),  # Include the users API URLs
 ]
--- resadmin/serializers.py
+++
@@ -1,8 +0,0 @@
-# resadmin/serializers.py  
-from rest_framework import serializers  
-from .models import Order  
-
-class OrderSerializer(serializers.ModelSerializer):  
-    class Meta:  
-        model = Order  
-        fields = '__all__' 
